86313d821f7a747c94f654afa576e5091921f8e9
Tokar Mykhailo <misatokar738@gmail.com>
2026-04-21 14:46:10 +0200

    cr hw 4

--- js/task-3.js
+++ js/task-3.js
@@ -18,5 +18,5 @@ console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 profile.changeUsername("Marco");
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-profile.changePlayTime(20);
+profile.updatePlayTime(20);
 console.log(profile.getInfo()); // "Marco has 320 active hours!"
